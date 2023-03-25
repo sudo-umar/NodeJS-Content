ee0ab4eabf57598ff6f7014c9550301eb06c239f
umar <umar@pop-os.localdomain>
2023-03-25 15:52:16 +0100

    Helper function

--- spaced-repetition-todo/spaced-todo.mjs
+++ spaced-repetition-todo/spaced-todo.mjs
@@ -3,20 +3,50 @@
 import open from 'open'
 import fetch from 'node-fetch'
 import yargs from 'yargs'
+import fs from 'fs'
+import chalk from 'chalk'
+import { info } from 'console'
 
-const {argv} = yargs(process.argv) 
-const res = await fetch('https://reddit.com/.json')
-let data = await res.json()
+// const {argv} = yargs(process.argv) 
+// const res = await fetch('https://reddit.com/.json')
+// let data = await res.json()
 
-const children = data.data.children 
-const random_post = children[Math.floor(Math.random()*children.length)]
-const link = `https://reddit.com${random_post.data.permalink}`
-if(argv.print){
-   console.log(`
-   data: ${random_post}
-   title: ${random_post.data.title}
-   link: ${link}
-   `) 
-}else{
-    open(link)
+// const children = data.data.children 
+// const random_post = children[Math.floor(Math.random()*children.length)]
+// const link = `https://reddit.com${random_post.data.permalink}`
+// if(argv.print){
+//    console.log(`
+//    data: ${random_post}
+//    title: ${random_post.data.title}
+//    link: ${link}
+//    `) 
+// }else{
+//     open(link)
+// }
+const file_name = process.cwd()+"/todo.txt" 
+const file_exists = fs.existsSync(file_name)
+if( file_exists === false){
+    fs.writeFileSync(file_name,"")
 }
+const sleep = (ms = 2000)=> new Promise((r)=> setTimeout(r,ms))
+const infoFunction = async ()=>{
+    const text = `
+    ${chalk.yellow('Commands :-')} 
+    ${chalk.bgGreen('todo')} add task1     # It will add task1 in todo list
+    ${chalk.bgGreen('todo')} ls            # It will show all tasks
+    ${chalk.bgGreen('todo')} done NUMBER   # Complete a todo
+    ${chalk.bgGreen('todo')} spaced NUMBER # That task will be remined in a spaced repetition
+                         manner 
+    `
+    console.log(text)
+    //await sleep()
+}
+
+const num = 10
+const warning = chalk.red()
+infoFunction()
+console.log(`
+
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
