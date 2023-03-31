1723ccb650e3aea820f35e32d3403af5ae1799a0
umar <umar@pop-os.localdomain>
2023-03-31 03:35:39 +0200

    cli basic setup working

---
+++ spaced-repetition-todo/info-module.mjs
@@ -0,0 +1,20 @@
+import chalk from 'chalk'
+import figlet from 'figlet'
+
+export const infoFunction = async()=>{
+    console.log(figlet.textSync('TODO!', {
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
+    const text = `
+    ${chalk.yellow('Commands :-')} 
+    ${chalk.bgGreen('todo')} add task1     # It will add task1 in todo list
+    ${chalk.bgGreen('todo')} list          # It will show all tasks
+    ${chalk.bgGreen('todo')} done NUMBER   # Complete a todo
+    ${chalk.bgGreen('todo')} spaced NUMBER # That task will be remined in a spaced repetion manner 
+    `
+    console.log(text)
+    //await sleep()
+}
--- spaced-repetition-todo/spaced-todo.mjs
+++ spaced-repetition-todo/spaced-todo.mjs
@@ -1,66 +1,33 @@
 #!/usr/bin/env node
 
-import open from 'open'
-import fetch from 'node-fetch'
-import yargs from 'yargs'
+// import open from 'open'
+// import fetch from 'node-fetch'
 import fs from 'fs'
 import chalk from 'chalk'
-import { info } from 'console'
 import { program } from 'commander'
+import { infoFunction } from './info-module.mjs'
 
-// const {argv} = yargs(process.argv) 
-// const res = await fetch('https://reddit.com/.json')
-// let data = await res.json()
-
-// const children = data.data.children 
-// const random_post = children[Math.floor(Math.random()*children.length)]
-// const link = `https://reddit.com${random_post.data.permalink}`
-// if(argv.print){
-//    console.log(`
-//    data: ${random_post}
-//    title: ${random_post.data.title}
-//    link: ${link}
-//    `) 
-// }else{
-//     open(link)
-// }
 const file_name = process.cwd()+"/todo.txt" 
 const file_exists = fs.existsSync(file_name)
 if( file_exists === false){
     fs.writeFileSync(file_name,"")
 }
-//const sleep = (ms = 2000)=> new Promise((r)=> setTimeout(r,ms))
-const infoFunction = async()=>{
-    const text = `
-    ${chalk.yellow('Commands :-')} 
-    ${chalk.bgGreen('todo')} add task1     # It will add task1 in todo list
-    ${chalk.bgGreen('todo')} list          # It will show all tasks
-    ${chalk.bgGreen('todo')} done NUMBER   # Complete a todo
-    ${chalk.bgGreen('todo')} spaced NUMBER # That task will be remined in a spaced repetion manner 
-    `
-    console.log(text)
-    //await sleep()
-}
+
 program
-    .name('todo in spaced repetition manner')
-    .description("CLI for todo tasks")
-    .version('0.0.1')
-program
-    .command('info')
-    .description('Print all available commands')
-    .action(infoFunction())
-program.parse()
+    .version("0.0.1")
+    .name("TODO Spaced Repetition")
+    .description("CLI for todo tasks in spaced repetition manner")
+    .option('-l, --ls [value]', 'list all tasks')
+    .option('-a, --add [value]', 'add a task')
+    .option('-d, --done [value]', 'mark a task as done')
+    // .option('h, --help', 'print all available commands')
+    .parse(process.argv)
 
+const options = program.opts()
 
-const addTask = async()=>{
-
+if(Object.keys(options).length === 0){
+    infoFunction()
 }
-//const num = 10
-//const warning = chalk.red()
-//infoFunction()
-
-console.log(`
-
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+if(options.ls){
+    console.log(options)
+}
